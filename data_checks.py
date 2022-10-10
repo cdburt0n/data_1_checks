e10ddbc1c39fed0db36aa31ada02af0d9f3e4fa2
Crystal Burton <cdburtn@gmail.com>
2022-10-09 20:58:53 -0400

    add solutions

--- data_checks.py
+++ data_checks.py
@@ -1,1 +1,9 @@
 print("hello world")
+numbers = [1,2,3,4]
+print(numbers)
+code = {"one":1,"two":2,"three":3}
+print(code)
+print(numbers[0])
+print(code["three"])
+colors = ("blue","green","red","orange")
+print(colors[3])
